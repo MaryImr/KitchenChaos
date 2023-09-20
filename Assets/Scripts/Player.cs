6e47652ed916724eaf4dfad9307922474b5238e0
Maryam Imran <maryamimran405@gmail.com>
2023-09-20 13:38:27 +0500

    Update player movement with properly animated rotations

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+
+   [SerializeField] private float moveSpeed = 7f;
+
    private void Update() {
       Vector2 inputVector = new Vector2(0, 0);
       if (Input.GetKey(KeyCode.W)) {
@@ -22,7 +25,9 @@ public class Player : MonoBehaviour
       inputVector = inputVector.normalized;
 
       Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
-      transform.position += moveDir * Time.deltaTime;
-      Debug.Log(inputVector);
+      transform.position += moveDir * moveSpeed * Time.deltaTime;
+
+      float rotateSpeed = 10f;
+      transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
    }
 }
